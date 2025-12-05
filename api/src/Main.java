984b0e29acba10b5a7937d722a236149050f4123
ignacio.tejera <ignacio.tejera@fing.edu.uy>
2025-12-05 12:04:29 -0300

    Agregar publicación de Web Services en Main.java y crear Dockerfile para la aplicación

--- api/src/Main.java
+++ api/src/Main.java
@@ -1,4 +1,5 @@
 import casosPrueba.CargaDatos;
+import jakarta.xml.ws.Endpoint;
 import webservices.publicadorEvento;
 import webservices.publicadorImagenes;
 import webservices.publicadorUsuario;
@@ -8,14 +9,29 @@ public class Main {
         publicadorEvento pubEvento = new publicadorEvento();
 		publicadorUsuario pubUsuario = new publicadorUsuario();
 		publicadorImagenes pubImagenes = new publicadorImagenes();
+
+        System.out.println("Cargando datos de prueba...");
+
         try {
             CargaDatos.cargarDatos(); 
         }
         catch (Exception e) {
             e.printStackTrace();
         }
-		pubEvento.publicar();
-		pubUsuario.publicar();
-		pubImagenes.publicar();
+        
+        System.out.println("Iniciando publicacion de Web Services...");
+
+        String host = System.getenv().getOrDefault("WS_HOST", "0.0.0.0");
+        String port = System.getenv().getOrDefault("WS_PORT", "8080");
+        String url = "http://" + host + ":" + port;
+
+        System.out.println("Publicando WS en: " + url);
+
+        Endpoint.publish(url + "/publicadorEvento", pubEvento);
+        Endpoint.publish(url + "/publicadorUsuario", pubUsuario);
+        Endpoint.publish(url + "/publicadorImagenes", pubImagenes);
+
+        System.out.println("WS de Evento, Usuario e Imagenes publicados correctamente.");
+        
     }
 }
